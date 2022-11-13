7f7b63a26d64618aab89e58151c57f08346bee37
ali <ali.s.tag2000@gmail.com>
2022-11-13 13:26:38 +0330

    inputs. first version.

---
+++ input.py
@@ -0,0 +1,23 @@
+input_values = {"general information": {"h": 10,
+                                        "delta h": 0.1
+                                        },
+                "point load": {
+                    "number": 1,
+                    "q": 1000,
+                    "l": 5,
+                    "teta": 0
+                },
+                "line load": {
+                    "number": 2,
+                    "q": 100,
+                    "l": 5,
+                    "q ": 100,
+                    "l ": 6
+                },
+                "strip load": {
+                    "number": 1,
+                    "q": 100,
+                    "l1": 5,
+                    "l2": 15
+                }
+                }
