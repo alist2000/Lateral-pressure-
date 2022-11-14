0454ab76d057b59cd1be45f6ab42b16f1867880f
ali <ali.s.tag2000@gmail.com>
2022-11-14 11:47:06 +0330

    traffic data.

--- input.py
+++ input.py
@@ -1,21 +1,36 @@
 input_values = {"general information": {"h": 10,
-                                        "delta h": 0.1
+                                        "delta h": 2
                                         },
                 "point load": {
-                    "number": 1,
-                    "q": 1000,
-                    "l": 5,
-                    "teta": 0
+                    "number": 6,
+                    "q": 16000,
+                    "l": 6,
+                    "teta": 66.8,
+                    "q ": 16000,
+                    "l ": 6,
+                    "teta ": 0,
+                    "q  ": 4000,
+                    "l  ": 5,
+                    "teta  ": 66.8,
+                    "q   ": 16000,
+                    "l   ": 12,
+                    "teta   ": 49.4,
+                    "q    ": 16000,
+                    "l    ": 12,
+                    "teta    ": 0,
+                    "q     ": 4000,
+                    "l     ": 12,
+                    "teta     ": 49.4
                 },
                 "line load": {
-                    "number": 2,
+                    "number": 0,
                     "q": 100,
                     "l": 5,
                     "q ": 100,
                     "l ": 6
                 },
                 "strip load": {
-                    "number": 1,
+                    "number": 0,
                     "q": 100,
                     "l1": 5,
                     "l2": 15
