883c5443a4e4d1e3836c762d8d6effc9571d9895
ali <ali.s.tag2000@gmail.com>
2022-11-16 15:54:59 +0330

    just for test.

---
+++ test.py
@@ -0,0 +1,24 @@
+import numpy as np
+from math import *
+from scipy import integrate
+import decimal
+
+# number = 1.2555
+# number2 = 554535.1
+# print(cos(np.pi))
+# deci = (decimal.Decimal(number).as_tuple())
+# print(deci.exponent)
+# print(deci.sign)
+# print(deci.digits)
+# print(str(number2)[::-1].find('.'))  # count number of decimal
+# _______________________ 15 aban 12:30 ____________________________
+# print(atan(1/1))
+# print(pow(10,-1))
+# _______________________ 22 aban 12:50 ____________________________
+a = np.array([0,1,2,3])
+b = np.array([0,1,2,3])
+c = np.array([i for i in range(4)])
+print(c)
+c = np.append(c,10)
+print(c)
+print(a*b)
