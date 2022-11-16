645da63f56dabdc45c3e06eb6933c209cb0ad739
ali <ali.s.tag2000@gmail.com>
2022-11-16 15:53:19 +0330

    new style of input according to input form of site.

--- input.py
+++ input.py
@@ -1,38 +1,75 @@
-input_values = {"general information": {"h": 10,
-                                        "delta h": 2
-                                        },
-                "point load": {
-                    "number": 6,
-                    "q": 16000,
-                    "l": 6,
-                    "teta": 66.8,
-                    "q ": 16000,
-                    "l ": 6,
-                    "teta ": 0,
-                    "q  ": 4000,
-                    "l  ": 5,
-                    "teta  ": 66.8,
-                    "q   ": 16000,
-                    "l   ": 12,
-                    "teta   ": 49.4,
-                    "q    ": 16000,
-                    "l    ": 12,
-                    "teta    ": 0,
-                    "q     ": 4000,
-                    "l     ": 12,
-                    "teta     ": 49.4
-                },
-                "line load": {
-                    "number": 0,
-                    "q": 100,
-                    "l": 5,
-                    "q ": 100,
-                    "l ": 6
-                },
-                "strip load": {
-                    "number": 0,
-                    "q": 100,
-                    "l1": 5,
-                    "l2": 15
-                }
+input_values_old_version = {"unit system": "us", "general information": {"h": 10,
+                                                                         "delta h": 0.1
+                                                                         },
+                            "point load": {
+                                "number": 6,
+                                "q": 16000,
+                                "l": 6,
+                                "teta": 66.8,
+                                "q ": 16000,
+                                "l ": 6,
+                                "teta ": 0,
+                                "q  ": 4000,
+                                "l  ": 5,
+                                "teta  ": 66.8,
+                                "q   ": 16000,
+                                "l   ": 12,
+                                "teta   ": 49.4,
+                                "q    ": 16000,
+                                "l    ": 12,
+                                "teta    ": 0,
+                                "q     ": 4000,
+                                "l     ": 12,
+                                "teta     ": 49.4
+                            },
+                            "line load": {
+                                "number": 0,
+                                "q": 100,
+                                "l": 5,
+                                "q ": 100,
+                                "l ": 6
+                            },
+                            "strip load": {
+                                "number": 0,
+                                "q": 100,
+                                "l1": 5,
+                                "l2": 15
+                            }
+                            }
+
+input_values = {"unit system": "us", "general information": {"h": 10,
+                                                             "delta h": 0.1,
+
+                                                             "Load Type": "Point Load",
+                                                             "q": 16000,
+                                                             "l1": 6,
+                                                             "teta": 66.8,
+
+                                                             "Load Type ": "Point Load",
+                                                             "q ": 16000,
+                                                             "l1 ": 6,
+                                                             "teta ": 0,
+
+                                                             "Load Type  ": "Line Load",
+                                                             "q  ": 100,
+                                                             "l1  ": 5,
+
+                                                             "Load Type   " : "Line Load",
+                                                             "q   ": 100,
+                                                             "l1   ": 6,
+
+                                                             "Load Type    ": "Strip Load",
+                                                             "q    ": 100,
+                                                             "l1    ": 5,
+                                                             "l2    ": 15,
+
+                                                             "Load Type     ": "Strip Load",
+                                                             "q     ": 100,
+                                                             "l1     ": 6,
+                                                             "l2     ": 16,
+
+                                                             "Load Type      ": "No Load",
+                                                             "Load Type       ": "No Load",
+                                                             "Load Type        ": "No Load"
+                                                             }
                 }
