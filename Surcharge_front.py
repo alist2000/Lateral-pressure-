367edbb6a162f148838d33fc1bbbfc408dfe0279
ali <ali.s.tag2000@gmail.com>
2022-11-21 11:06:05 +0330

    create html function for sometimes that there are some errors. edit bfp and now this output look better.

--- Surcharge_front.py
+++ Surcharge_front.py
@@ -213,6 +213,181 @@ def generate_html_response_surcharge(output):
     return export
 
 
+def generate_html_response_surcharge_no_solution(output):
+    html = "<html>"
+    html_end = "</html>"
+
+    head = """<head>
+	<title>Output Summary</title>
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+	<style type="text/css">
+		* {font-size: 10px; }
+		body {
+			font: 10px Arial, Helvetica, sans-serif;
+			line-height: 1.0;
+		}
+		@media print {
+			.pagebreak { page-break-before: always; }
+		}
+		@page {
+			size: letter;
+			margin: 1.5cm;
+			@bottom-right {
+				content: "Page " counter(page) " of " counter(pages);
+			}
+		}
+		.custom-page-start {
+			margin-top: 0px;
+			margin-bottom: 0px;
+		}
+		h1 {color: #2f4f6e; font-size: 15px;}
+		h2 {
+			background: #96B9D0; font-size: 13px;
+			padding-top: 3px;
+			padding-bottom: 3px;
+			padding-left: 3px;
+			margin-bottom: 5px; margin-top: 5px;
+			margin-left: -1px; margin-right: -1px;
+		}
+		h3 {
+			background: #84c1ff; font-size: 13px;
+			font-size: 10px; 
+			margin-bottom: -1px; margin-top: -1px;
+			margin-left: -1px; margin-right: -1px;
+			padding-top: 8px;
+			padding-bottom: 8px;
+			padding-left: 8px;
+		}
+		t1 {display: block; font-size: 15px; font-style: italic; padding-left: 3px;}
+		t1b {font-size: 15px; font-style: italic; font-weight: bold;}
+		t2 {font-size: 15px;}
+		t2b {font-size: 15px;font-weight: bold;}
+		p {font-size: 10px;}
+		td {vertical-align: top;}
+	</style>
+  </head>"""
+
+    body = "<body>"
+    body_end = "</body>"
+    div = """<div class="custom-page-start"> <hr>"""
+    div_start = """<div class="custom-page-start" style="width: 50%; : padding-right: 5px">"""
+    div_end = "</div>"
+    h1 = "<h1>"
+    h1_title = "<h1 style='font-size: 15px'>"
+    h1_end = "</h1>"
+    table = "<table border=1>"
+    table_end = "</table>"
+    tbody = "<tbody>"
+    tbody_end = "</tbody>"
+    hr = "<hr>"
+    tr = "<tr>"
+    tr_end = "</tr>"
+    th = "<th>"
+    th_end = "</th>"
+    td = "<td>"
+    td_end = "</td>"
+
+    def title():
+
+        t1 = """		<table border="0" style="border-collapse: collapse; width: 100%;">
+			<tbody>
+				<tr>
+					<td style="width: 100%;font-size: 15px;">
+						<h2>"""
+
+        t2 = """</h2>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    <hr>"""
+
+        title = h1_title + output[0][0] + h1_end + t1 + output[0][1] + t2
+
+        return title
+
+    def solutions():
+        m1 = """
+      <table border="0" style="border-collapse: collapse; width: 100%;">
+          <tbody>
+            <tr>
+              <td style="width: 100%;"><t1b></t1b></td>
+            </tr>
+          </tbody>
+        </table>
+          """
+
+        h1 = """
+          <table border="0" style="border-collapse: collapse; width: 100%; background: #dfe3e6">
+			<tbody>
+				<tr>
+					<td style="width: 100%;padding: 5px;border: 5px solid white;background-color: #bfd6f6  ;"><t2b>
+          """
+        h2 = """
+          </t2b></td>
+          </tr>
+        </tbody>
+      </table>
+          """
+
+        h3 = """
+        <table border="1" bordercolor="#C0C0C0" style="border-collapse: collapse; width: 100%; background: #dfe3e6">
+			<tbody style="width: 100%;padding: 5px;border: 5px solid white;background-color: #e7eff6 ">
+          """
+
+        h4 = """
+          </tbody>
+      </table>
+          """
+
+        h8 = f"""
+        <td style="width: 10%;text-align: center;" ><t2>
+        """
+        h8_1 = f"""
+                <td style="width: 10%;text-align: center;" colspan="{output[1][0] - 1}"><t2>
+                """
+
+        h9 = """
+        </t2></td>
+        """
+
+        s = ""
+        print(range(len(output[4])))
+        for k in range(len(output[4])):
+            c1 = 0
+            c2 = 0
+            c3 = 0
+            # Table Solution Group Fields, Field labels, units and values
+            for i in range(0, int(len(output[1]))):
+                if i % 2 == 0:
+                    s = s + h1 + str(output[1][i + 1]) + h2 + h3
+                    s += tr
+                    for j in range(output[1][i]):
+                        if j == 0:
+                            s = s + h8 + output[2][c1] + h9
+                        else:
+                            s = s + h8_1 + output[2][c1] + h9
+                            break
+                        c1 = c1 + 1
+                    s += tr_end
+                    for j in range(output[1][i]):
+                        if j == 0:
+                            s = s + tr
+                        s = s + h8 + str(output[4][k][c3]) + h9
+                        c3 = c3 + 1
+                    s += tr_end
+                    s = s + tbody_end + table_end
+            s = s + m1 + hr
+
+        return s
+
+    export = html + head + body + div + title() + solutions() + \
+             div_end + body_end + html_end
+
+    return export
+
+
 # no change till 10:49 - 11 september
 def generate_html_response_BFP_multi(output):
     response = ""
@@ -928,16 +1103,21 @@ def generate_html_response_BFP_multi_no_solution(output, project_num):
     return export
 
 
-print(generate_html_response_surcharge(
-    [['lateral pressure calculator - Output Summary', 'Final Solution Alternatives'], [2, 'lateral pressure'],
-     ['P', 'Z'], ['ft', 'psf'], [[59.05754899182392, 5.240666749714153], [730.4136771302904, 5.240666749714152],
-                                 [51.20000000013475, 4.419679486201468], [47.0588235294766, 4.807874904431918],
-                                 [362.23787162668754, 5.5337053025043135], [322.6494400247262, 5.700840631253652],
-                                 [1572.6173613031392, 5.3628981892403065]],
-     ['p14u10_Solution1_Surcharge_Summary_Report', 'p14u10_Solution1_Surcharge_Detailed_Report',
-      'p14u10_Solution2_Surcharge_Summary_Report', 'p14u10_Solution2_Surcharge_Detailed_Report',
-      'p14u10_Solution3_Surcharge_Summary_Report', 'p14u10_Solution3_Surcharge_Detailed_Report',
-      'p14u10_Solution4_Surcharge_Summary_Report', 'p14u10_Solution4_Surcharge_Detailed_Report',
-      'p14u10_Solution5_Surcharge_Summary_Report', 'p14u10_Solution5_Surcharge_Detailed_Report',
-      'p14u10_Solution6_Surcharge_Summary_Report', 'p14u10_Solution6_Surcharge_Detailed_Report',
-      'p14u10_Solution7_Surcharge_Summary_Report', 'p14u10_Solution7_Surcharge_Detailed_Report']]))
+# print(generate_html_response_surcharge(
+#     [['lateral pressure calculator - Output Summary', 'Final Solution Alternatives'], [2, 'lateral pressure'],
+#      ['P', 'Z'], ['ft', 'psf'], [[59.05754899182392, 5.240666749714153], [730.4136771302904, 5.240666749714152],
+#                                  [51.20000000013475, 4.419679486201468], [47.0588235294766, 4.807874904431918],
+#                                  [362.23787162668754, 5.5337053025043135], [322.6494400247262, 5.700840631253652],
+#                                  [1572.6173613031392, 5.3628981892403065]],
+#      ['p14u10_Solution1_Surcharge_Summary_Report', 'p14u10_Solution1_Surcharge_Detailed_Report',
+#       'p14u10_Solution2_Surcharge_Summary_Report', 'p14u10_Solution2_Surcharge_Detailed_Report',
+#       'p14u10_Solution3_Surcharge_Summary_Report', 'p14u10_Solution3_Surcharge_Detailed_Report',
+#       'p14u10_Solution4_Surcharge_Summary_Report', 'p14u10_Solution4_Surcharge_Detailed_Report',
+#       'p14u10_Solution5_Surcharge_Summary_Report', 'p14u10_Solution5_Surcharge_Detailed_Report',
+#       'p14u10_Solution6_Surcharge_Summary_Report', 'p14u10_Solution6_Surcharge_Detailed_Report',
+#       'p14u10_Solution7_Surcharge_Summary_Report', 'p14u10_Solution7_Surcharge_Detailed_Report']]))
+print(generate_html_response_surcharge_no_solution(
+    [['lateral pressure calculator - Output Summary', 'Final Solution Alternatives'], [3, 'Error!'],
+     ['Number of errors', 'Description'], [],
+     [[2, "Delta h can't be larger than h! Change your input!", "h can't be <= 0!"]]]
+    ))
