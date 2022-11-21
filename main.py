caca8e5f351c72b9b8d063203d632da05f4c8b22
ali <ali.s.tag2000@gmail.com>
2022-11-21 12:19:19 +0330

    1) draw final plot ( line number 124) 2) create "plots" list for plot objects.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from input import input_values
 from surchargeLoad import surcharge
 import scipy.integrate as spi
+from plot import plotter2
 from output import output, output_noSolution
 
 import numpy as np
@@ -20,6 +21,7 @@ def surcharge_calculator(input_values):
     depth_array = np.array(depth_list)
     solution = []
     load_type = []
+    plots = []
 
     # pl --> point load
     pl_q = []
@@ -40,26 +42,31 @@ def surcharge_calculator(input_values):
     i_ll = 0
     i_sl = 0
     for i in range(max_load_number):
-        load_type_dict = json.loads(input_values.get("data").get("Load Properties").get("Load Type" + i * spaceNum).get("value")).get("item")
+        load_type_dict = json.loads(
+            input_values.get("data").get("Load Properties").get("Load Type" + i * spaceNum).get("value")).get("item")
         load_type.append(load_type_dict)
         if load_type[i] == "Point Load":
             pl_q.append(float(input_values.get("data").get("Load Properties").get("q" + i * spaceNum).get("value")))
             pl_l.append(float(input_values.get("data").get("Load Properties").get("L1" + i * spaceNum).get("value")))
             pl_teta.append(float(input_values.get("data").get("Load Properties").get("Ɵ" + i * spaceNum).get("value")))
-            solution_pl.append(surchargeInstance.point_load(pl_q[i_pl], pl_l[i_pl], pl_teta[i_pl]))
+            point_load = surchargeInstance.point_load(pl_q[i_pl], pl_l[i_pl], pl_teta[i_pl])
+            plots.append(point_load[3])  # plot index = 3
+            solution_pl.append(point_load)
             i_pl += 1
         elif load_type[i] == "Line Load":
             ll_q.append(float(input_values.get("data").get("Load Properties").get("q" + i * spaceNum).get("value")))
             ll_l.append(float(input_values.get("data").get("Load Properties").get("L1" + i * spaceNum).get("value")))
-            print(ll_q)
-            print(ll_l)
-            solution_ll.append(surchargeInstance.line_load(ll_q[i_ll], ll_l[i_ll]))
+            line_load = surchargeInstance.line_load(ll_q[i_ll], ll_l[i_ll])
+            plots.append(line_load[3])  # plot index = 3
+            solution_ll.append(line_load)
             i_ll += 1
         elif load_type[i] == "Strip Load":
             sl_q.append(float(input_values.get("data").get("Load Properties").get("q" + i * spaceNum).get("value")))
             sl_l1.append(float(input_values.get("data").get("Load Properties").get("L1" + i * spaceNum).get("value")))
             sl_l2.append(float(input_values.get("data").get("Load Properties").get("L2" + i * spaceNum).get("value")))
-            solution_sl.append(surchargeInstance.strip_load(sl_q[i_sl], sl_l1[i_sl], sl_l2[i_sl]))
+            strip_load = surchargeInstance.strip_load(sl_q[i_sl], sl_l1[i_sl], sl_l2[i_sl])
+            plots.append(strip_load[3])  # plot index = 3
+            solution_sl.append(strip_load)
             i_sl += 1
         else:
             pass  # no load
@@ -71,13 +78,13 @@ def surcharge_calculator(input_values):
     # Check for errors
     for load_type in solution:
         for load in load_type:
-            if load[3][0] != "No error":
-                Output = output_noSolution(product_id, user_id, load[3])
+            if load[4][0] != "No error":
+                Output = output_noSolution(product_id, user_id, load[4])
                 return Output
 
     """ to drawing final sigma h - z plot we should sum all sigma h array of every load.
         for this result I create an array with length equal to length of sigma h array
-         of every solution ( index number 3 of every solution ( solution pl or ll or sl )
+         of every solution ( index number 2 of every solution ( solution pl or ll or sl )
           is sigma_h_array and all of them have same shape. ).
         and 0 value.then I write a loop to sum sigma h array of every load with first 
         sigma_h_array ( sum_sigma_h that has 0 value ).
@@ -113,9 +120,10 @@ def surcharge_calculator(input_values):
     print(lateral_pressure)
     print(sum_sigma_h)
     print(centroid)
-    surchargeInstance.total_plotter(lateral_pressure, centroid, sum_sigma_h)
-
-    Output = output(product_id, user_id, solution, sum_sigma_h, depth_array, lateral_pressure, centroid, unit_system)
+    # surchargeInstance.total_plotter(lateral_pressure, centroid, sum_sigma_h)
+    result_plot = plotter2(unit_system, h, sum_sigma_h, depth_list, lateral_pressure, centroid)
+    plots.append(result_plot)
+    Output = output(product_id, user_id, solution, sum_sigma_h, depth_array, lateral_pressure, centroid, unit_system, plots)
     return Output
 
 
