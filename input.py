54c1a5fa9da2af45744f60de85fc1ed74faba124
ali <ali.s.tag2000@gmail.com>
2022-11-21 09:37:38 +0330

    add input from site.

--- input.py
+++ input.py
@@ -37,39 +37,58 @@ input_values_old_version = {"unit system": "us", "general information": {"h": 10
                             }
                             }
 
-input_values = {"product_id": 14, "user_id": 10, "unit system": "us", "general information": {"h": 10,
-                                                                                              "delta h": 0.01,
+input_values_old = {"product_id": 14, "user_id": 10, "unit system": "us", "general information": {"h": 10,
+                                                                                                  "delta h": 0.01,
 
-                                                                                              "Load Type": "Point Load",
-                                                                                              "q": 16000,
-                                                                                              "l1": 6,
-                                                                                              "teta": 66.8,
+                                                                                                  "Load Type": "Point Load",
+                                                                                                  "q": 16000,
+                                                                                                  "l1": 6,
+                                                                                                  "teta": 66.8,
 
-                                                                                              "Load Type ": "Point Load",
-                                                                                              "q ": 16000,
-                                                                                              "l1 ": 6,
-                                                                                              "teta ": 0,
+                                                                                                  "Load Type ": "Point Load",
+                                                                                                  "q ": 16000,
+                                                                                                  "l1 ": 6,
+                                                                                                  "teta ": 0,
 
-                                                                                              "Load Type  ": "Line Load",
-                                                                                              "q  ": 100,
-                                                                                              "l1  ": 5,
+                                                                                                  "Load Type  ": "Line Load",
+                                                                                                  "q  ": 100,
+                                                                                                  "l1  ": 5,
 
-                                                                                              "Load Type   ": "Line Load",
-                                                                                              "q   ": 100,
-                                                                                              "l1   ": 6,
+                                                                                                  "Load Type   ": "Line Load",
+                                                                                                  "q   ": 100,
+                                                                                                  "l1   ": 6,
 
-                                                                                              "Load Type    ": "Strip Load",
-                                                                                              "q    ": 100,
-                                                                                              "l1    ": 5,
-                                                                                              "l2    ": 15,
+                                                                                                  "Load Type    ": "Strip Load",
+                                                                                                  "q    ": 100,
+                                                                                                  "l1    ": 5,
+                                                                                                  "l2    ": 15,
 
-                                                                                              "Load Type     ": "Strip Load",
-                                                                                              "q     ": 100,
-                                                                                              "l1     ": 6,
-                                                                                              "l2     ": 16,
+                                                                                                  "Load Type     ": "Strip Load",
+                                                                                                  "q     ": 100,
+                                                                                                  "l1     ": 6,
+                                                                                                  "l2     ": 16,
 
-                                                                                              "Load Type      ": "No Load",
-                                                                                              "Load Type       ": "No Load",
-                                                                                              "Load Type        ": "No Load"
-                                                                                              }
-                }
+                                                                                                  "Load Type      ": "No Load",
+                                                                                                  "Load Type       ": "No Load",
+                                                                                                  "Load Type        ": "No Load"
+                                                                                                  }
+                    }
+
+input_values = {
+    'information': {'title': '1', 'jobNo': '1', 'designer': '1', 'checker': '1', 'company': '1', 'client': '1',
+                    'unit': 'us', 'date': None, 'comment': None, 'other': None}, 'product_id': 25, 'user_id': 44,
+    'data': {'Load Properties': {'H': {'value': '10', 'unit': 'ft'}, 'Δh': {'value': '0.01', 'unit': None},
+                                 'Load Type': {
+                                     'value': '{"id":1237,"item":"Point Load","section_product_item":"37_84"}',
+                                     'unit': None}, 'q': {'value': '1500', 'unit': None},
+                                 'L1': {'value': '5', 'unit': 'ft'}, 'Ɵ': {'value': '0', 'unit': None}, 'Load Type ': {
+            'value': '{"id":1243,"item":"Strip Load","section_product_item":"37_84"}', 'unit': None},
+                                 'q ': {'value': '4000', 'unit': None}, 'L1 ': {'value': '4', 'unit': 'ft'},
+                                 'L2 ': {'value': '5', 'unit': 'ft'}, 'Load Type  ': {
+            'value': '{"id":1248,"item":"Line Load","section_product_item":"37_84"}', 'unit': None},
+                                 'q  ': {'value': '123', 'unit': None}, 'L1  ': {'value': '4', 'unit': 'ft'},
+                                 'Load Type   ': {
+                                     'value': '{"id":1253,"item":"Strip Load","section_product_item":"37_84"}',
+                                     'unit': None}, 'q   ': {'value': '111', 'unit': None},
+                                 'L1   ': {'value': '2', 'unit': 'ft'}, 'L2   ': {'value': '3', 'unit': 'ft'}}},
+    'type': 1, 'number_of_projects': 1}
