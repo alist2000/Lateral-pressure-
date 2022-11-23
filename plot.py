f3acd57f93085817b9fb2a2db639a89091dcd7ac
ali <ali.s.tag2000@gmail.com>
2022-11-23 15:36:27 +0330

    edit style of plot. change color and add dimension line. not finished yet.

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.graph_objs import Layout
 
 
 def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
@@ -12,13 +13,22 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
         lenght_unit = "m"
     depth_array = np.array(depth_list)
     sigma_h_array = np.array(sigma_h)
-    plot = px.line(y=depth_array, x=sigma_h_array).update_layout(title_text="Lateral Pressure",
-                                                                 xaxis_title=f"Ϭh ({pressure_unit})",
-                                                                 yaxis_title=f"Z ({lenght_unit})",
-                                                                 xaxis={"side": "top"})
+    plot = px.line(y=depth_array, x=sigma_h_array, color_discrete_sequence=["#595959"]).update_layout(
+        xaxis_title=f"Ϭh ({pressure_unit})",
+        yaxis_title=f"Z ({lenght_unit})",
+        xaxis={"side": "top",
+               "zeroline": True,
+               "mirror": "ticks",
+               "zerolinecolor": "#969696",
+               "zerolinewidth": 4, },
+        yaxis={"zeroline": True,
+               "mirror": "ticks",
+               "zerolinecolor": "#969696",
+               "zerolinewidth": 4}
+    )
     j = int(len(depth_list) / 5)
     plot['layout']['yaxis']['autorange'] = "reversed"
-    plot['layout']['xaxis']['range'] = [-0.5, max(sigma_h_array) + 0.5]
+    plot['layout']['xaxis']['range'] = [-max(sigma_h_array)/60, max(sigma_h_array) + max(sigma_h_array)/60]
 
     # show maximum
     plot.add_traces(
@@ -27,8 +37,10 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
             mode="markers", name="Max", hoverinfo="skip"
         )
     )
-    plot.update_traces(marker={'size': 15})
+    # color and size of maximum point
+    plot.update_traces(marker=dict(color='red', size=8), legendgroup="maximum")
 
+    # show some vector to look better
     result = go.layout.Annotation(dict(
         x=0.01,
         y=centroid,
@@ -39,8 +51,8 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
         ax=max(sigma_h_array),
         ay=centroid,
         arrowhead=3,
-        arrowwidth=4,
-        arrowcolor='rgb(255,51,0)', )
+        arrowwidth=3,
+        arrowcolor='#cccccc', )
     )
     arrow1 = go.layout.Annotation(dict(
         x=0.01,
@@ -53,7 +65,7 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
         ay=depth_list[j],
         arrowhead=3,
         arrowwidth=1.5,
-        arrowcolor='rgb(0,128,255)', )
+        arrowcolor='#595959', )
     )
     arrow2 = go.layout.Annotation(dict(
         x=0.01,
@@ -66,7 +78,7 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
         ay=depth_list[2 * j],
         arrowhead=3,
         arrowwidth=1.5,
-        arrowcolor='rgb(0,128,255)', )
+        arrowcolor='#595959', )
     )
     arrow3 = go.layout.Annotation(dict(
         x=0.01,
@@ -79,7 +91,7 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
         ay=depth_list[3 * j],
         arrowhead=3,
         arrowwidth=1.5,
-        arrowcolor='rgb(0,128,255)', )
+        arrowcolor='#595959', )
     )
     arrow4 = go.layout.Annotation(dict(
         x=0.01,
@@ -92,7 +104,7 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
         ay=depth_list[4 * j],
         arrowhead=3,
         arrowwidth=1.5,
-        arrowcolor='rgb(0,128,255)', )
+        arrowcolor='#595959', )
     )
     arrow5 = go.layout.Annotation(dict(
         x=0.01,
@@ -105,10 +117,27 @@ def plotter2(unit_system, h, sigma_h, depth_list, lateral_pressure, centroid):
         ay=depth_list[5 * j],
         arrowhead=3,
         arrowwidth=1.5,
-        arrowcolor='rgb(0,128,255)', )
+        arrowcolor='#595959', )
     )
     list_of_all_arrows = [result, arrow1, arrow2, arrow3, arrow4, arrow5]
 
     plot.update_layout(annotations=list_of_all_arrows)
-    plot.show()
+
+    # add annotation for result force
+    plot.add_traces((
+        go.Line(x=[max(sigma_h_array) + 0.5], y=[0], mode='markers + text',
+                marker=dict(size=15, symbol='star-triangle-up-dot')),
+        go.Line(x=[max(sigma_h_array) + max(sigma_h_array) / 80], y=[centroid], mode='markers + text',
+                marker=dict(size=15, symbol='star-triangle-down-dot')),
+        go.Line(x=[max(sigma_h_array) + max(sigma_h_array) / 80, max(sigma_h_array) + max(sigma_h_array) / 80],
+                y=[0, centroid])))
+    plot.update_traces(showlegend=False, marker=dict(color="#cccccc", size=5), legendgroup="dimention")
+
+    # changing background color
+    layout = Layout(
+        paper_bgcolor='#ffffff',
+        plot_bgcolor='#ffffff'
+    )
+    plot.update_layout(layout)
+    # plot.show()
     return plot
